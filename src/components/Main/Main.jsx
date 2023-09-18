ca067fc2cf2cda57f3e1c6a64cb6d2e7044263aa
garvitthakan <101065700+garvitthakan@users.noreply.github.com>
2023-09-18 23:06:31 +0530

    Updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,37 +1,52 @@
-import React, { useContext } from 'react'
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core'
+import React, { useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
 
-import { ExpenseTrackerContext } from '../../context/context'
+import { ExpenseTrackerContext } from '../../context/context';
 import InfoCard from '../InfoCard';
 
 const Main = () => {
     const classes = useStyles();
     const { balance } = useContext(ExpenseTrackerContext);
+
+    // Define the URL you want to link to
+    const externalSiteUrl = 'https://ai-wallet-investment.onrender.com/';
+
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Now handle your expense at ease" />
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance : ₹{balance}</Typography>
+                <Typography align="center" variant="h5">Total Balance: ₹{balance}</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     <InfoCard />
                 </Typography>
-                <Divider className={classes.divider}/>
+                <Divider className={classes.divider} />
                 <Form />
             </CardContent>
             <CardContent className={classes.CardContent}>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} >
+                    <Grid item xs={12}>
                         <List />
                     </Grid>
                 </Grid>
+            </CardContent>
 
+            {/* Add the button with the hyperlink */}
+            <CardContent>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    href={externalSiteUrl}  // Specify the URL here
+                >
+                    AI Investment Tool
+                </Button>
             </CardContent>
         </Card>
-    )
+    );
 }
 
-export default Main
+export default Main;
